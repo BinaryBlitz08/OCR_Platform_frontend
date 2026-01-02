b836ed4247a40773256231805ca62523224409f0
Bhavya Bharadwaj <binaryblitz08@gmail.com>
2026-01-02 22:17:23 +0530

    feat: add dashboard with batch upload, preview, and download buttons

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { OcrService } from '../../services/ocr';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  imports: [CommonModule,FormsModule],
   selector: 'app-dashboard',
-  imports: [],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrls: ['./dashboard.css']
 })
 export class Dashboard {
+  selectedFiles: FileList | null = null;
+  results: any[] = [];
+  loading = false;
+  message = '';
 
+  constructor(private ocrService: OcrService) {}
+
+  onFileSelected(event: any) {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload() {
+    if (!this.selectedFiles || this.selectedFiles.length === 0) return;
+
+    this.loading = true;
+    this.message = '';
+    this.results = [];
+
+    this.ocrService.uploadImages(this.selectedFiles).subscribe({
+      next: (res) => {
+        this.results = res.results || [];
+        this.message = 'Upload successful!';
+        this.loading = false;
+      },
+      error: (err) => {
+        this.message = err.error.message || 'Upload failed';
+        this.loading = false;
+      }
+    });
+  }
+
+  download(fileId: string, type: string) {
+    this.ocrService.downloadFile(fileId, type);
+  }
 }
